7efec9c6d3eea0cad00ab3500f1ec0718652a3b9
beroid <beroid3@gmail.com>
2018-01-10 09:55:33 +0500

    Revert "mm: writeback: every 500ms the writeback daemon kicks in and flushes the dirty pages into disk."
    This reverts commit a3785a32b700994fbf91d32af111e880a9f6257c.

--- mm/page-writeback.c
+++ mm/page-writeback.c
@@ -36,7 +36,6 @@
 #include <linux/pagevec.h>
 #include <linux/mm_inline.h>
 #include <trace/events/writeback.h>
-#include <linux/earlysuspend.h>
 
 #include "internal.h"
 
@@ -57,8 +56,6 @@
 #define BANDWIDTH_INTERVAL	max(HZ/5, 1)
 
 #define RATELIMIT_CALC_SHIFT	10
-
-bool resume = true;
 
 /*
  * After a CPU has dirtied this many pages, balance_dirty_pages_ratelimited
@@ -99,7 +96,7 @@ unsigned long vm_dirty_bytes;
 /*
  * The interval between `kupdate'-style writebacks
  */
-unsigned int dirty_writeback_interval = 0; /* centiseconds */
+unsigned int dirty_writeback_interval = 5 * 100; /* centiseconds */
 
 EXPORT_SYMBOL_GPL(dirty_writeback_interval);
 
@@ -1594,21 +1591,6 @@ static struct notifier_block __cpuinitdata ratelimit_nb = {
 	.next		= NULL,
 };
 
-static void dirty_early_suspend(struct early_suspend *handler)
-{
-	dirty_writeback_interval = 5 * 100;
-}
-
-static void dirty_late_resume(struct early_suspend *handler)
-{
-	dirty_writeback_interval = 0;
-}
-
-static struct early_suspend dirty_suspend = {
-	.suspend = dirty_early_suspend,
-	.resume = dirty_late_resume,
-};
-
 /*
  * Called early on to tune the page writeback dirty limits.
  *
@@ -1630,8 +1612,6 @@ static struct early_suspend dirty_suspend = {
 void __init page_writeback_init(void)
 {
 	int shift;
-	
-	register_early_suspend(&dirty_suspend);
 
 	writeback_set_ratelimit();
 	register_cpu_notifier(&ratelimit_nb);
